942bf671071b17e8f4ff89bd3ee502910a784e07
mirzarafi5678 <mirzasufianrafi5678@gmail.com>
2026-05-31 20:50:01 +0600

    Letter Combinations of a Phone Number problem

---
+++ Letter Combinations of a Phone Number.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        phone = {
+            "2": "abc", "3": "def", "4": "ghi",
+            "5": "jkl", "6": "mno", "7": "pqrs",
+            "8": "tuv", "9": "wxyz"
+        }
+
+        result = []
+
+        def backtrack(index, path):
+            if index == len(digits):
+                result.append(path)
+                return
+
+            letters = phone[digits[index]]
+            for letter in letters:
+                backtrack(index + 1, path + letter)
+
+        backtrack(0, "")
+        return result
+
+
+ss= Solution()
+ss.letterCombinations("23")
+print(ss.letterCombinations(""))
---
+++ phone map/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        Map<Integer , String>  values = new HashMap<>(Map.of(
+                0, " ",
+                1,"abc",
+                2,"yoyo"
+        ));
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+public class Main{
+
+    public static void main(String[] args) {
+
+    }
+}
