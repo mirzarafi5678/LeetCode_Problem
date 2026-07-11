5c66902b8310b806fbe4e80c5e40a146e5ee2034
mirzarafi5678 <mirzasufianrafi5678@gmail.com>
2026-07-11 19:55:11 +0600

    problem no 36

--- valid Soduko/src/Main.java
+++ valid Soduko/src/Main.java
@@ -1,50 +1,94 @@
 import java.util.ArrayList;
 
 class  Solution {
-    public boolean isValidSudoku(char[][] board) {
 
+    boolean rowWiseChecl(char [][] board){
         ArrayList<Character> checker = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
 
-        for (int i = 0; i < 9; i++) {
             checker.clear();
 //            char []  row = board[i];
             int j = 0;
-            while ( j<9){
-
-                if (!checker.contains(board[i][j])) {
-                    checker.add(board[i][j]);
-                }
-                else {
-                    return false;
-                }
+            while ( j<board.length){
+              if (board[i][j] != '.') {
+                  if (!checker.contains(board[i][j])) {
+                      checker.add(board[i][j]);
+                  } else {
+                      return false;
+                  }
+              }
 
                 j++;
 
 
+
             }
-
-
-            
-
         }
 
-        for (int i = 0; i < 9; i++) {
+        return true;
+    }
+
+    boolean columnWiseCheck(char[][] board){
+        ArrayList<Character> checker = new ArrayList<>();
+        for (int j = 0; j < board.length; j++) {
             checker.clear();
-            int j =0 ;
-            while (j <9){
 
-                checker.add(board[i][j])
-                j++;
+            for (int i = 0; i <  board.length ; i++) {
 
+              if(board[i][j] != '.'){
+                if ( checker.contains(board[i][j])  ){
 
+                    return false;
+                }else {
+                    checker.add(board[i][j]);
+
+                }
+              }
             }
-
         }
-
-
 
 
         return true;
+
+    }
+
+
+   boolean boxWiseCheck(char [][] board){
+        ArrayList<Character> checker = new ArrayList<>();
+        for (int row = 0; row < 9; row+=3 ) {
+           for (int col = 0; col < 9; col+=3) {
+               checker.clear();
+
+               for (int i = row; i < row +3; i++) {
+                   for (int j = row; j < col+3; j++) {
+                       if (board[i][j] !='.'){
+                           if (checker.contains(board[i][j])){
+                               return false;
+
+                           }
+                           checker.add(board[i][j]);
+
+
+                       }
+                   }
+               }
+
+
+
+           }
+       }
+        return true;
+    }
+
+
+    public boolean isValidSudoku(char[][] board) {
+
+
+
+        return rowWiseChecl(board) && columnWiseCheck(board) && boxWiseCheck(board);
+
+
+
     }
 }
 
